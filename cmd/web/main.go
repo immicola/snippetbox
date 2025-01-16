7e3589054c08339e5025dc7cd2dda645a101c202
immicola <toktasynmiras29@gmail.com>
2025-01-16 10:33:51 +0500

    2.09

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,10 +7,14 @@ import (
 
 func main() {
 	mux := http.NewServeMux()
+
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+
 	mux.HandleFunc("GET /{$}", home)
 	mux.HandleFunc("GET /snippet/view/{id}", snippetView)
 	mux.HandleFunc("GET /snippet/create", snippetCreate)
 	mux.HandleFunc("POST /snippet/create", snippetCreatePost)
+	mux.Handle("GET /static/", fileServer)
 
 	log.Print("starting server on :4000")
 
